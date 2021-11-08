d261cfd95156c3dc043e6c5d7d1167eaad625cf8
Michael Sean De La Riva <michaeldlr17@gmail.com>
2021-11-08 12:03:40 -0800

    still working with commerceJS to increment items and add items into the store. committing current changes to test live. -Michael De La Riva

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,24 +3,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product.jsx';
 import useStyles from './styles.js';
 
-const products = [
-    { 
-        id: 1, 
-        name: 'Shoes', 
-        description: 'Running shoes', 
-        price: '$5', 
-        image: 'https://www.hoka.com/dw/image/v2/BDJD_PRD/on/demandware.static/-/Sites-HOKA-US-master/default/dw14f1b0ee/images/transparent/1113513-WEPR_1.jpg?sw=414&sfrm=png&q=0&bgcolor=FFFFFF' 
-    },
-    { 
-        id: 2, 
-        name: 'Macbook', 
-        description: 'Apple Macbook', 
-        price: '$10', 
-        image: 'https://www.apple.com/newsroom/images/product/mac/standard/Apple_16-inch-MacBook-Pro_111319_big.jpg.large.jpg' 
-    },
-];
 
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
     return(
         <main className={classes.content}>
@@ -28,7 +12,7 @@ const Products = () => {
             <Grid container justifyContent='center' spacing={4}>
                 {products.map((products) => (
                     <Grid item key={products.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={products} />
+                        <Product product={products} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
