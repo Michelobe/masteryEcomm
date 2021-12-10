fcde68991081a205084495feed20e97737d8f653
Michael Sean De La Riva <michaeldlr17@gmail.com>
2021-12-10 11:04:10 -0800

    changed ./lib/commerce.js file to include key for live testing. -Michael De La Riva

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+export const commerce = new Commerce("pk_35624d611f965fea52137fc186a11c13a0e841bbf6349", true);
